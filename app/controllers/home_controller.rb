8509b21905047c28ab6ae37aae02fee9a33cb740
admin <admin@localhost.localdomain>
2019-07-09 21:16:44 +0900

    second commit

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,58 @@
+# 以下のrequireは、railsの自動require機能により不要になる(!)
+=begin
+require 'net/http'
+require 'uri'
+require 'json'
+=end
+
+require 'net/http'
+require 'uri'
+require 'json'
+
 class HomeController < ApplicationController
   def top
   end
+
+  def show
+    # hash形式でパラメタ文字列を指定し、URL形式にエンコード
+    #params = URI.encode_www_form({zipcode: '7830060'})
+    # URIを解析し、hostやportをバラバラに取得できるようにする
+    #uri = URI.parse("http://zipcloud.ibsnet.co.jp/api/search?#{params}")
+
+    uri = URI.parse("https://api.openweathermap.org/data/2.5/weather?lat=35&lon=13&appid=6d6cc69b9c9eb779ca25c4b18182b33a")
+
+    @uriString = uri
+    # リクエストパラメタを、インスタンス変数に格納
+    @query = uri.query
+
+    response = Net::HTTP.get_response(uri)
+
+    # 例外処理の開始
+    begin
+      # responseの値に応じて処理を分ける
+      case response
+      # 成功した場合
+      when Net::HTTPSuccess
+        # responseのbody要素をJSON形式で解釈し、hashに変換
+        @result = JSON.parse(response.body)
+        # 表示用の変数に結果を格納
+      # 別のURLに飛ばされた場合
+      when Net::HTTPRedirection
+        @message = "Redirection: code=#{response.code} message=#{response.message}"
+      # その他エラー
+      else
+        @message = "HTTP ERROR: code=#{response.code} message=#{response.message}"
+      end
+    # エラー時処理
+    rescue IOError => e
+      @message = e.message
+    rescue TimeoutError => e
+      @message = e.message
+    rescue JSON::ParserError => e
+      @message = e.message
+    rescue => e
+      @message = e.message
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   ## get 'home/top'
   get "/" => "home#top"
   get "post/index" => "post#index"
-  post "post/create" => "create"
+  post "post/create" => "post#create"
+
+  get "show" => "home#show"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
